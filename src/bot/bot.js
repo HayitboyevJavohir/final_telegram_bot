b6fe35b6777770e58496481143dd5c73967bf977
Javohir Hayitboyev <javohirhayitboyev@gmail.com>
2025-12-06 12:27:22 +0500

    onStart

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,17 +1,23 @@
 import TelegramBot from "node-telegram-bot-api";
 import { config } from "dotenv";
+import onStart from "./handlers/onStart.js";
+import onProfile from "./handlers/onProfile.js";
 config();
 
 const TOKEN = process.env.BOT_TOKEN;
 
-const bot = new TelegramBot(TOKEN, {polling: true});
+ export const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on("message", function (msg) {
     const chatId = msg.chat.id;
     const firstname = msg.chat.first_name;
     const text = msg.text;
-    bot.sendMessage(chatId, `Assalomu aleykum, ${firstname}`);
-    bot.sendMessage(chatId, `${text}`);
+  
+    if (text == "/start") {
+        return onProfile(msg);
+    }
+
+    return bot.sendMessage(chatId, `Kutilmagan xatolik... /start bosing!`)
 });
 
 console.log(`Bot ishga tushdi...`);
