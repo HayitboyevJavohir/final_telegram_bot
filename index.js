41d607eb760e216a22e38210b4c2555ff710f091
Javohir Hayitboyev <javohirhayitboyev@gmail.com>
2025-12-16 11:22:34 +0500

    birinchi commit

--- index.js
+++ index.js
@@ -1,15 +1,84 @@
-import "./src/bot/bot.js";
+// import TelegramBot from "node-telegram-bot-api";
+import TelegramBot from "node-telegram-bot-api";
+import { config } from "dotenv";
+import { onElse } from "./src/onElse.js";
+import { onCourses } from "./src/onCourse.js";
+import { onStart } from "./src/onStart.js";
+import { onRegister } from "./src/onRegister.js";
 import mongoose from "mongoose";
+import User from "./src/models/User.js";
+import onUsers from "./src/onUser.js";
 
+config();
+
+const TOKEN = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+// db connection
 mongoose
- .connect(process.env.MONGO_URI)
- .then(() => {
-    console.log(`Db is connected...`);
-    
- })
- .catch(() => {
-    console.log(`Error: db is not connected!`);
-    
- })
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("db is connected...");
+  })
+  .catch(() => {
+    console.log(`Error: db is not connected...!`);
+  });
 
-console.log(`Dastur ishga tushmoqda...`);
+bot.on("message", (msg) => {
+  // console.log(msg);
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const firstName = msg.chat.first_name;
+
+  if (text == "/start") {
+    onStart(chatId, firstName);
+  } else if (text == "📚 Kurslar") {
+    onCourses(chatId);
+  } else if (text == "✍️ Ro‘yxatdan o‘tish") {
+    onRegister(chatId);
+  } else if (text == "/users") {
+    onUsers(chatId);
+  } else {
+    onElse(chatId);
+  }
+});
+
+bot.on("callback_query", (query) => {
+  console.log(query);
+  const chatId = query.message.chat.id;
+  const firstName = query.message.chat.first_name;
+  const data = query.data;
+
+  if (data == "course_english") {
+    bot.sendMessage(
+      chatId,
+      `
+    🇬🇧 Ingliz tili kursi haqida:
+
+📆 Davomiyligi: 3 oy  
+⏰ Darslar: Haftasiga 3 marta (1,5 soatdan)  
+👨‍🏫 O‘qituvchi: Tajribali filologlar  
+💰 Narxi: 450 000 so‘m / oy
+
+✍️ Agar sizni bu kurs qiziqtirsa, “Ro‘yxatdan o‘tish” tugmasini bosing.
+
+    `,
+      {
+        reply_markup: {
+          keyboard: [
+            [{ text: "✍️ Ro‘yxatdan o‘tish" }],
+            [{ text: "⬅️ Orqaga" }],
+          ],
+          resize_keyboard: true,
+        },
+      }
+    );
+  }
+
+  // bot.sendMessage(chatId, data);
+});
+
+console.log("Bot ishga tushdi");
+
+export { bot };
--- src/models/User.js
+++ src/models/User.js
@@ -1,26 +1,19 @@
 import mongoose from  "mongoose";
 
 const userSchema = new mongoose.Schema({
-    chatId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    firstname: {
-        type: String,
-    },
-    username: {
-        type: String,
-        default: null,
-    },
-    active: {
-        type: Boolean,
-        default: true,
-    },
-    balance: {
-        type: Number,
-        default: 2000,
-    },
+  telegramId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  firstname: {
+    type: String,
+    default: "foydalanuvchi",
+  },
+  balance: {
+    type: Number,
+    default: 4000,
+  },
 });
 
 const User = new mongoose.model("User", userSchema);
